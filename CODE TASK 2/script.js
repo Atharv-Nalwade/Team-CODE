6c6220a35313f3871ec053af55930aaf6f655e3e
Darky001 <98139553+Atharv-Nalwade@users.noreply.github.com>
2022-09-14 00:03:39 +0530

    Update script.js

--- CODE TASK 2/script.js
+++ CODE TASK 2/script.js
@@ -5,6 +5,12 @@ function fun(){
 document.body.style.background = faker.color.rgb() ;
 }
 
+/*
+var rgbColor = 'rgb(46, 123, 14)';
+rgbArr = rgbColor.substring(4, rgbColor.length-1).replace(/ /g, '').split(',');
+console.log(rgbArr);
+
+*/
 
 
 function func1(){
@@ -12,6 +18,11 @@ function func1(){
 let rand2 = Math.random() * (255 - 0) + 0;
 let rand3 = Math.random() * (255 - 0) + 0;
   //document.body.style.background =rgb(rand1,rand2,rand3);
+  // var test=document.getElementById('change1');
+  // console.log( window.getComputedStyle(test));
+  
+  
+  
   document.body.style.background = 'rgb(' + rand1 + ',' + rand2 + ',' + rand3 + ')';
 }
 
